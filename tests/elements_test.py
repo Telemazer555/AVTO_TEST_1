2630a86e0236124135090a794719b682ee622321
Forsworn555 <Forsworn555@yandex.ru>
2023-02-18 12:31:47 +0600

    init comit

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -9,5 +9,10 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
             text_box_page.open()
-            text_box_page.fill_all_fields()
-            time.sleep(15)
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            time.sleep(5)
+            output_name, output_email, output_cur_addr, output_per_addr = text_box_page.chek_filled_from()
+            assert full_name == output_name
+            assert email == output_email
+            assert current_address == output_cur_addr
+            assert permanent_address == output_per_addr
