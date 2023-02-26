88aa5fed2c1ed2ea0a481cf9f51963bdefce83d2
Forsworn555 <Forsworn555@yandex.ru>
2023-02-26 19:05:51 +0600

    Webtables с добавлением рандомных людей и asert по поиску

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -9,6 +11,11 @@ def generated_person():
     yield Person(
         full_name=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
         email=faker_ru.email(),
+        firstname=faker_ru.first_name(),
+        lastname=faker_ru.last_name(),
+        age=random.randint(10, 80),
+        department=faker_ru.job(),
+        salary=random.randint(100, 1000),
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address(),
     )
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,7 @@
+import random
 import time
 
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
 
 
 class TestElements:
@@ -46,3 +47,24 @@ class TestElements:
                 assert output_no == 'No'
                 assert output_impressive == 'Impressive'
 
+    class TestWebTable:
+
+        def test_web_table_add_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            new_person = web_table_page.add_new_person()
+            table_result = web_table_page.check_new_added_person()
+            assert new_person in table_result
+
+        def test_web_table_search_person(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            key_word = web_table_page.add_new_person()[random.randint(0, 5)]
+            # ( Добавил задержку time.sleep(1) ибо не успевает отработать следующая строчка)
+            time.sleep(1)
+            web_table_page.search_some_person(key_word)
+            #time.sleep(1)
+            table_result = web_table_page.check_search_person()
+            print(key_word)
+            print(table_result)
+            assert key_word in table_result
